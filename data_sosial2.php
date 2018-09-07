82328add81eacef23b696d92fbbce60900228ca1
denfafa <43058179+denfafa@users.noreply.github.com>
2018-09-07 17:10:18 +0700

    Add files via upload

---
+++ data_sosial2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Website Padukuhan Sembuku</title>
+<meta name="description" content="Pedukuhan Sembuku">
+<META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="revisit-after" content="1 days">
+<meta name="distribution" content="web">
+<meta http-equiv="refresh" content="30">
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="menu.css" rel="stylesheet" type="text/css" />
+<!--[if lt IE 7]>
+<style type="text/css" media="screen">
+#menuh{float:none;}
+body{behavior:url(csshover.htc); font-size:75%;}
+#menuh ul li{float:left; width: 100%;}
+#menuh a{height:1%;font:normal 1em/1.6em  helvetica,  "Trebuchet MS", arial, sans-serif;}
+</style>
+<![endif]-->
+</head>
+<body>
+<div id="top">
+  <h2><a href="http://sembuku.rf.gd">homepage</a> | <a href="mailto:padukuhan.sembuku@gmail.com">contact</a></h2>
+</div>
+<div id="banner">
+  <h1> <a href="http://sembuku.dadapayu.ooo">Website Padukuhan sembuku</a></h1>
+</div>
+<div id="menuh-container">
+  <div id="menuh">
+    <ul>
+      <li><a href="http://sembuku.dadapayu.ooo" class="top_parent">HOME</a> </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">PENDIDIKAN</a>
+        <ul>
+          <li><a href="data_pendidikan1.php">Data 1</a></li>
+          <li><a href="data_pendidikan2.php">Data 2</a></li>
+          <li><a href="data_pendidikan3.php">Data 3</a></li>
+          <li><a href="data_pendidikan4.php">Data 4</a></li>
+          <li><a href="data_pendidikan5.php">Data 5</a></li>
+          <li><a href="data_pendidikan6.php">Data 6</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">EKONOMI</a>
+        <ul>
+          <li><a href="data_ekonomi1.php">Data 1</a></li>
+          <li><a href="data_ekonomi2.php">Data 2</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">SOSIAL</a>
+        <ul>
+          <li><a href="data_sosial1.php">Data 1</a></li>
+          <li><a href="data_sosial2.php">Data 2</a></li>
+          <li><a href="data_sosial3.php">Data 3</a></li>
+          <li><a href="data_sosial4.php">Data 4</a></li>
+          <li><a href="data_sosial5.php">Data 5</a></li>
+          <li><a href="data_sosial6.php">Data 6</a></li>
+          </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">KEPENDUDUKAN</a>
+        <ul>
+          <li><a href="data_angka_rasio_ketergantungan.php">Data 1</a></li>
+          <li><a href="jumlah_penduduk_berdasar_Jenis_kelamin.php">Data 2</a></li>
+          <li><a href="jumlah_penduduk_berdasarkan_umur.php">Data 3</a></li>
+	    </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">Contact</a> </li>
+    </ul>
+  </div>
+</div>
+    </ul>
+        </div>
+  <div id="content">
+    <h3><span style="color:#710069;font-family: georgia,sans-serif;font-weight: bold;">Welcome to Padukuhan Sembuku Website</span></h3>
+    <blockquote></a>.</blockquote>
+    <p></p>
+    <h3> News &amp; Updates</h3>
+    <div 
+      <h4>Data jumlah PUS berdasarkan kelompok umur istri di Padukuhan Sembuku</h4>
+      <p><iframe style="overflow:hidden;border:0;margin:0;padding:0;width:500px;height:500px;" src="data/sosial2.xlsx.html"></iframe><p>
+  </div>
+</div>
+<div id="footer"> <a href="">homepage</a> | <a href="mailto:fafa.sosiatri@gmail.com">contact</a> | <a href="http://validator.w3.org/check?uri=referer">html</a> | <a href="http://jigsaw.w3.org/css-validator">css</a> | &copy; 2018 Padukuhan Sembuku | Design by <a href="https://www.facebook.com/dvd.rf.online"> Raden FaFa</a> | This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a> </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Website Padukuhan Sembuku</title>
+<meta name="description" content="Pedukuhan Sembuku">
+<META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="revisit-after" content="1 days">
+<meta name="distribution" content="web">
+<meta http-equiv="refresh" content="30">
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="menu.css" rel="stylesheet" type="text/css" />
+<!--[if lt IE 7]>
+<style type="text/css" media="screen">
+#menuh{float:none;}
+body{behavior:url(csshover.htc); font-size:75%;}
+#menuh ul li{float:left; width: 100%;}
+#menuh a{height:1%;font:normal 1em/1.6em  helvetica,  "Trebuchet MS", arial, sans-serif;}
+</style>
+<![endif]-->
+</head>
+<body>
+<div id="top">
+  <h2><a href="http://sembuku.rf.gd">homepage</a> | <a href="mailto:padukuhan.sembuku@gmail.com">contact</a></h2>
+</div>
+<div id="banner">
+  <h1> <a href="http://sembuku.rf.gd">Website Padukuhan sembuku</a></h1>
+</div>
+<div id="menuh-container">
+  <div id="menuh">
+    <ul>
+      <li><a href="http://sembuku.dadapayu.ooo" class="top_parent">HOME</a> </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">PENDIDIKAN</a>
+        <ul>
+          <li><a href="data_pendidikan1.php">Data 1</a></li>
+          <li><a href="data_pendidikan2.php">Data 2</a></li>
+          <li><a href="data_pendidikan3.php">Data 3</a></li>
+          <li><a href="data_pendidikan4.php">Data 4</a></li>
+          <li><a href="data_pendidikan5.php">Data 5</a></li>
+          <li><a href="data_pendidikan6.php">Data 6</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">EKONOMI</a>
+        <ul>
+          <li><a href="data_ekonomi1.php">Data 1</a></li>
+          <li><a href="data_ekonomi2.php">Data 2</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">SOSIAL</a>
+        <ul>
+          <li><a href="data_sosial1.php">Data 1</a></li>
+          <li><a href="data_sosial2.php">Data 2</a></li>
+          <li><a href="data_sosial3.php">Data 3</a></li>
+          <li><a href="data_sosial4.php">Data 4</a></li>
+          <li><a href="data_sosial5.php">Data 5</a></li>
+          <li><a href="data_sosial6.php">Data 6</a></li>
+          </ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">KEPENDUDUKAN</a>
+        <ul>
+          <li><a href="data_angka_rasio_ketergantungan.php">Data 1</a></li>
+          <li><a href="jumlah_penduduk_berdasar_Jenis_kelamin.php">Data 2</a></li>
+          <li><a href="jumlah_penduduk_berdasarkan_umur.php">Data 3</a></li>
+		</ul>
+      </li>
+    </ul>
+    <ul>
+      <li><a href="#" class="top_parent">Contact</a> </li>
+    </ul>
+  </div>
+</div>
+    </ul>
+  </div>
+</div>
+<div id="container">
+  <div id="sidebar">
+    <ul>
+      <h3>CATEGORIES</h3>
+    <ul>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/13-Aspirasi-Warga">ASPIRASI WARGA</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/1-BERITA">BERITA</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/2-FASILITAS">FASILITAS</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/10-KEGIATAN">KEGIATAN</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/3-KESEHATAN">KESEHATAN</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/4-LINGKUNGAN">LINGKUNGAN</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/5-PENDIDIKAN">PENDIDIKAN</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/7-RELIGI">RELIGI</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/8-SEJARAH">SEJARAH</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/12-SOSIAL-BUDAYA">SOSIAL BUDAYA</a></li>
+      <li><a href="http://sembuku.rf.gd/index.php?/categories/9-POTENSI-DESA">POTENSI DESA</a></li>
+      <li><a href="data_sarana_prasarana.php">SARANA IBADAH</a></li>
+      <li><a href="data_sebaran_ruang_public.php">RUANG PUBLIC</a></li>
+      <ul>
+      <h3>LINK</h3>
+    <ul>  
+      <li><a href="http://sembuku.rf.gd/">Blog Padukuhan sembuku</a></li>
+	  <li><a href="">Web Padukuhan Sembuku</a></li>
+	  <li><a href="http://padukuhan-mojo.rf.gd/">Blog Padukuhan Mojo</a></li>
+	  <li><a href="http://mojo.rf.gd/">Web Padukuhan Mojo</a></li>
+	  <li><a href="http://sembuku.rf.gd/index.php?/admin">ADMIN LOGIN</a></li>
+      <li><a href="http://dadapayu-semanu.desa.id/">WEB DESA DADAPAYU</a></li>
+      <li><a href="http://gunungkidulkab.go.id/">WEB PORTAL GUNUNGKIDUL</a></li>
+    <ul>
+    </ul>
+    <h3>Widget</h3>
+    <ul>
+    <p><a href="https://www.worldflagcounter.com/details/e1A"><img src="https://www.worldflagcounter.com/e1A/" alt="Flag Counter"></a></p>  
+    <p><iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&size=small&timezone=Asia%2FJakarta" width="150%" height="150" frameborder="0" seamless></iframe><p>
+      
+      </div>
+  <div id="content">
+    <h3><span style="color:#710069;font-family: georgia,sans-serif;font-weight: bold;">Welcome to Padukuhan sembuku Website</span></h3>
+    <blockquote></a>.</blockquote>
+    <p></p>
+    <h3> News &amp; Updates</h3>
+    <div class="scroll">
+      <h4>August 9 2018</h4>
+      <p>Pada tanggal 9 Agustus 2018 Setelah sosialisasi Ekonomi Kreatif di Padukuhan Sembuku, maka Ibu Astuti selaku Dukuh memutuskan untuk membentuk UPPKS yang bernama Sekar Kinanthi. Akan tetapi Bapak Sunoto pada saat itu memberikan saran nama UPPKS Amanah, karena dinilai bagus maknanya. Setelah pembentukan struktur UUPKS selesai setelah itu Ibu Dukuh dan Bapak Sunoto meresmikan UPPKS Amanah di Padukuhan Sembuku.</p>
+      <h4>August 9 2018</h4>
+      <p>Pemerintah mensosialisasikan UPPKS untuk menumbuhkan kepedulian dan komitmen pada stakeholder, mitra kerja, serta masyarakat pada program Pemberdayaan Ekonomi Keluarga.</p>
+      
+    </div>
+    <h3>Articles</h3>
+    <p> Pembentukan dan Pengaktifan UPPKS
+Program Pemberdayaan Ekonomi Keluarga yang bertujuan meningkatkan kesejahteraan keluarga dilaksanakan melalui usaha mikro. Untuk itu, UPPKS sebagai wadah yang melaksanakan program tersebut harus dibentuk dan diaktifkan. Dalam hal ini, UPPKS dibentuk dan diaktifkan untuk meningkatkan pemberdayaan usaha mikro.
+Peningkatan Tenaga Terampil Anggota UPPKS Pelatihan ketrampilan teknis anggota kelompok UPPKS dalam mengelola dan mengembangkan usahanya.</p>
+    
+    </div>
+    
+    
+  </div>
+</div>
+<div id="footer"> <a href="">homepage</a> | <a href="mailto:fafa.sosiatri@gmail.com">contact</a> | <a href="http://validator.w3.org/check?uri=referer">html</a> | <a href="http://jigsaw.w3.org/css-validator">css</a> | &copy; 2018 Padukuhan Sembuku | Design by <a href="http://radenfafa.blogs.or.id"> Raden FaFa</a> | This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a> </div>
+</body>
+</html>
